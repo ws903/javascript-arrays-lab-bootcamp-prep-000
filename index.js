3546075ca2ed8ba1ffe546fe808450cfc258a143
David Sim <wsim903@gmail.com>
2018-08-08 18:07:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,9 @@ function destructivelyRemoveLastKitten(){
 }
 
 function removeLastKitten(){
-  return kittens.slice(0, kittens.length)
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
 }
