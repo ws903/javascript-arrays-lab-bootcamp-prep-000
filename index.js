05f21d56eb89c407d6eab015d37af8e518f57234
David Sim <wsim903@gmail.com>
2018-08-08 17:57:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your functions and code here
-var kittens = ['Milo', 'Otis', 'Garfield']
+// var kittens = ['Milo', 'Otis', 'Garfield']
 
 function descructivelyAppendKitten(name){
   kittens.push(name)
-  return kittens
+  // return kittens
 }
